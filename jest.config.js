8eb89ab8d8a08a07efcbe10d29258ac87de2a6eb
Ian De Bie <ian@debie.us>
2025-07-01 21:18:08 -0500

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+    },
+    moduleNameMapper: {
+        // Handle CSS imports (if you ever add them)
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
 };
-
